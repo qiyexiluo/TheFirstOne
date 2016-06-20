734ebc5c845ae16b0419bd98335df63c43bc7580
lanouhn <lanouhn@zhangbo.local>
2016-06-20 15:52:20 +0800

    gdsygeh45yybhz45hbysw4b5hysw4bh4wsbhbhfwsbhy54wyhbt5r4wsbhyu

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by lanouhn on 16/6/20.
+//  Copyright © 2016年 七叶昔洛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by lanouhn on 16/6/20.
+//  Copyright © 2016年 七叶昔洛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
